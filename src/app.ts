3cf298c15418c272ef2bc132e33d46faa04bc1a3
rui-watanabe <wataru610806@gmail.com>
2020-06-06 10:45:30 +0900

    node routing with type

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
 // import * as express from 'express';
-import express = require('express');
+// import express = require('express');
+import express, { Request, Response, NextFunction } from 'express';
 const app = express();
+app.use((req, res, next) => {
+  console.log('hello');
+  next();
+});
+
+app.get('/', (req, res, next) => {
+  res.send('<h1>hello</h1>')
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  next();
+});
 app.listen(3000);
 
