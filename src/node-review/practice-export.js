2255847db4ceaa110054a8d4d298d1046e3ba6c0
rui-watanabe <wataru610806@gmail.com>
2020-06-06 09:55:23 +0900

    review node module

---
+++ src/node-review/practice-export.js
@@ -0,0 +1,13 @@
+// executed default
+// module.exports = {
+//   hello: 'hello'
+// }
+// exports = module.exports;
+// module.exports = {}
+
+module.exports.hello = 'hello';
+exports.name = 'Max';
+
+// module.exports = function () {
+//   return 'hello'
+// }
