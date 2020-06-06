47264c3bb32ead1b74cc942babc3ab6bc47d28f9
rui-watanabe <wataru610806@gmail.com>
2020-06-06 10:50:40 +0900

    node routing with type extends

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 // import * as express from 'express';
 // import express = require('express');
 import express, { Request, Response, NextFunction } from 'express';
+
+interface MessageRequest extends Request {
+  body: {
+    message: string;
+  }
+}
+
 const app = express();
+
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log('hello');
   next();
@@ -9,6 +19,10 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res, next) => {
   res.send('<h1>hello</h1>')
+});
+
+app.post('/', (req: MessageRequest, res, next) => {
+  res.send(`<h1>I got ${req.body}</h1>`);
 });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
